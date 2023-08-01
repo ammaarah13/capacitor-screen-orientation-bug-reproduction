aa69177db57f148fa02521b9baf19f862fb98573
ammaarah <ammaarah@truevolve.technology>
2023-08-01 16:12:29 +0200

    screen orientation on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { ScreenOrientation } from "@capacitor/screen-orientation";
 
 @Component({
   selector: 'app-home',
@@ -10,4 +11,36 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomePage {
   constructor() {}
+
+  screenorientation = '';
+  locked = true;
+  buttonText = 'UNLOCK ORIENTATION'
+
+  ngOnInit() {
+    ScreenOrientation.lock({ orientation: 'portrait-primary' });
+    this.screenorientation = 'portrait-primary';
+  }
+
+  ionViewWillEnter() {
+    ScreenOrientation.addListener("screenOrientationChange", ({ type }) => {
+      this.screenorientation = type;
+    });
+  }
+
+  ionViewWillLeave() {
+    ScreenOrientation.removeAllListeners();
+  }
+
+  lockAndUnlock() {
+    if (this.locked === true) {
+      ScreenOrientation.unlock();
+      this.locked = false;
+      this.buttonText = 'LOCK ORIENTATION'
+    }
+    else {
+      ScreenOrientation.lock({ orientation: 'portrait-primary' });
+      this.locked = true;
+      this.buttonText = 'UNLOCK ORIENTATION'
+    }
+  }
 }
