226c6157d1dcffad57766acfc8ebfe868598d6b5
ammaarah <ammaarah@truevolve.technology>
2023-08-01 15:58:03 +0200

    initial commit

---
+++ android/app/src/main/java/capacitor/screen/orientation/bug/reproduction/MainActivity.java
@@ -0,0 +1,5 @@
+package capacitor.screen.orientation.bug.reproduction;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'capacitor.screen.orientation.bug.reproduction',
+  appName: 'capacitor-screen-orientation-bug-reproduction',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
